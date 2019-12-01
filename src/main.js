d17a431957106baa5714cf44f56198989d4dbe30
rhryhoryk <eh480.35@gmail.com>
2019-12-01 20:34:04 +0300

    fix:

--- src/main.js
+++ src/main.js
@@ -31,7 +31,7 @@ const tasks = generateTasks(TASK_COUNT);
 render(taskListElement, createTaskEditTemplate(tasks[0]));
 
 let showingTasksCount = SHOWING_TASKS_COUNT_ON_START;
-tasks.slice(1, showingTasksCount).forEach((task) => render(taskListElement, createTaskTemplate(task), `beforeend`));
+tasks.slice(1, showingTasksCount).forEach((task) => render(taskListElement, createTaskTemplate(task)));
 
 const boardElement = siteMainElement.querySelector(`.board`);
 
@@ -44,7 +44,7 @@ loadMoreButton.addEventListener(`click`, () => {
   showingTasksCount = showingTasksCount + SHOWING_TASKS_COUNT_BY_BUTTON;
 
   tasks.slice(prevTasksCount, showingTasksCount)
-    .forEach((task) => render(taskListElement, createTaskTemplate(task), `beforeend`));
+    .forEach((task) => render(taskListElement, createTaskTemplate(task)));
 
   if (showingTasksCount >= tasks.length) {
     loadMoreButton.remove();
